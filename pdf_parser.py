0d796a74bb0e75e3abf8cad12c62b315a072a53c
Siddhardha Moparthi <siddhardha.moparthi@uconn.edu>
2017-04-02 19:55:51 -0400

    started fixing conventions

--- pdf_parser.py
+++ pdf_parser.py
@@ -15,11 +15,12 @@ class PdfParser:
         self.eng = enchant.Dict("en_US")
 
     def run(self, _download_url):
-        pdf_name = self.download_file(_download_url)
-        # pdf_name = "input.pdf"
+        # pdf_name = self.download_file(_download_url)
+        pdf_name = "input.pdf"
         raw_text = self.convert(pdf_name)
         visible_text = self.prime_text(raw_text)
-        self.process(visible_text)
+        FinalList = self.process(visible_text)
+        self.search_list(FinalList)
 
 
     def prime_text(self, raw_text):
@@ -30,7 +31,6 @@ class PdfParser:
         #     f.write(soup)
         # os.remove("input.pdf")
         return soup
-
 
     def download_file(self, download_url):
         """
@@ -98,12 +98,12 @@ class PdfParser:
             else:
                 fixed_list.append(word)
 
-        print(fixed_list)
-        for i in range(0, len(fixed_list)):
-            current_word = fixed_list[i]
-            if not self.eng.check(current_word):
-                if self.is_relevant(fixed_list, i):
-                    print(current_word)
+        return fixed_list
+        # for i in range(0, len(fixed_list)):
+        #     current_word = fixed_list[i]
+        #     if not self.eng.check(current_word):
+        #         if self.is_relevant(fixed_list, i):
+        #             print(current_word)
                 # pattern1 = re.compile(r'MK.+')
                 # pattern2 = re.compile(r'V\d.+')
                 # if re.match(pattern1, current_word):
@@ -123,6 +123,78 @@ class PdfParser:
                 if re.match("phase", main_list[x], re.IGNORECASE):
                     return True
         return False
+
+
+    def search_list(self, FinalList2):
+        iterat=len(FinalList2)
+        FirstFilter = []
+        company_name = "Merck"
+        for x in range(0, iterat):
+            newstr4 = FinalList2[x].replace(",", "")
+            newstr3 = newstr4.replace(":", "")
+            newstr2 = newstr3.replace("<", "")
+            newstr1 = newstr2.replace(">", "")
+          #  newstr7 = newstr1.replace("'", "")
+            newstr9 = newstr1.replace("]", "")
+            newstr10 = newstr9.replace("[", "")
+            newstr = newstr10.replace(")", "")
+            #newstr = newstr10.replace("", "")
+
+            sizeCheck=len(newstr)
+
+            if sizeCheck!=0:
+
+                appe = self.eng.check(newstr)
+
+                if appe==False:
+                    temp=len(newstr)
+                    if temp>=5:
+                        temps=FinalList2.count(FinalList2[x])
+                        if temps>=1:
+                            if company_name in newstr:
+                               # print('Processing...')
+                                pass
+                            elif "/" in newstr:
+                                newstr.split("/")
+                                permA=self.eng.check(newstr[0])
+                                permB=self.eng.check(newstr[1])
+                                if permA & permB==False:
+                                    news=self.checking_official(newstr)
+                                    if news!='null':
+                                        self.FirstFilter.append(news)
+
+                            else:
+                                news=self.checking_official(newstr)
+                                if news!='null':
+                                    FirstFilter.append(news)
+
+        #apple=eng.check(FinalList[x])
+        SecondFilter=list(set(FirstFilter))
+        print(SecondFilter)
+
+    def checking_official(self, currentCheck):
+        p1 = re.compile(r"\b[A-Za-z]+(vir|cillin|mab|ximab|zumab|tinib|vastatin|prazole|lukast|axine|olol|oxetine|sartan|pril|pib|oxacin|xaban|afil|ine|parib|tide)\b")
+        p2 = re.compile(r"\b[A-Za-z]+(grel|barb|prost)[A-Za-z]+\b")
+        p3 = re.compile(r"\b(cef)[A-Za-z]+\b")
+        p4 = re.compile(r"[A-Z].+\d$")
+        p5 = re.compile(r'V\d.+')
+        drug_target="N/A"
+        currentLast=len(currentCheck);
+
+        if re.match(p1, currentCheck): # Suffix testing
+            return currentCheck
+        elif re.match(p2, currentCheck): # Infix testing
+            return currentCheck
+        elif re.match(p3, currentCheck): # Prefix testing
+            return currentCheck
+        elif re.match(p5, currentCheck): # Prefix testing
+            return currentCheck
+        elif re.match(p4, currentCheck): # Number ending
+            drug_target="Some Weird Number Thing"
+            return currentCheck
+
+        return 'null'
+
 
 
 
